866a11ff3be89ca3720b324e5edc4a767774b561
NolionsChen <nolionschen@jetfuel.com.tw>
2018-10-03 17:51:11 +0800

    [revise] 新增程式註解

--- client.go
+++ client.go
@@ -10,14 +10,11 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// var msg string
-
 func main() {
 	fmt.Println("Cliner runing...")
 
 	// create ecoonection
 	conn, _, err := websocket.DefaultDialer.Dial("ws://127.0.0.1:8080/echo", nil)
-
 	if err != nil {
 		log.Println("dail error: ", err)
 	}
@@ -28,22 +25,25 @@ func main() {
 func run(c *websocket.Conn) {
 	defer c.Close()
 
+	// create a buffer
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Print("Please input a data:")
 
+		// get input data form buffer
 		msg, _ := reader.ReadString('\n')
 		msg = strings.TrimSpace(msg)
 
+		// send data by wen scoket
 		err := c.WriteMessage(websocket.TextMessage, []byte(msg))
 		if err != nil {
 			log.Println("write:", err)
 			return
 		}
 
+		// get response msg form web scoket
 		_, echo, err := c.ReadMessage()
-
 		if err != nil {
 			log.Println("read:", err)
 			return
