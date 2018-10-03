51f4510df2c4af3f835c0e4939331e62b5123c36
NolionsChen <nolionschen@jetfuel.com.tw>
2018-10-03 15:55:04 +0800

    [feature] init

---
+++ README.md
@@ -0,0 +1,10 @@
+# install package
+
+    dep ensure
+
+# run server  
+    go run server.go
+
+# use by browser
+
+open http://127.0.0.1:8080 
---
+++ server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	fmt.Println("Server Starting...")
+
+	flag.Parse()
+
+	setRoutes()
+
+	http.ListenAndServe("localhost:8080", nil)
+}
+
+func setRoutes() {
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/", index)
+
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	var upgrader = websocket.Upgrader{}
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Printf("read: %s", err)
+			break
+		}
+
+		log.Println("recv:", string(message))
+
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	httpTemplate := template.Must(template.ParseFiles("index.html"))
+	httpTemplate.Execute(w, "ws://"+r.Host+"/echo")
+}
